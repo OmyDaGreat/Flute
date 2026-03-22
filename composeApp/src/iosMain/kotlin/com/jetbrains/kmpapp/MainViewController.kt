6c6704291c16046bd2e11ced3526530a4fa66f74
Om Gupta <om@malefic.xyz>
2026-03-22 08:40:17 -0700

    feat: init from mobile template

---
+++ composeApp/src/iosMain/kotlin/com/jetbrains/kmpapp/MainViewController.kt
@@ -0,0 +1,6 @@
+package com.jetbrains.kmpapp
+
+import androidx.compose.ui.window.ComposeUIViewController
+
+@Suppress("ktlint:standard:function-naming", "unused", "FunctionName")
+fun MainViewController() = ComposeUIViewController { App() }
